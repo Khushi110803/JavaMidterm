a6ad2245f48260f3de5e7f6e7246b1b31a06fe59
Khushi110803 <200539535@student.georgianc.on.ca>
2023-10-16 11:08:09 -0400

    Question 2b Complete

--- src/main/java/com/example/midterm/DBUtility.java
+++ src/main/java/com/example/midterm/DBUtility.java
@@ -57,4 +57,31 @@ public class DBUtility {
 
         return employees;
     }
+    public static void removeEmployee(int employeeId) {
+        try {
+            // Load the MySQL JDBC driver and establish a connection to the database.
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            Connection connection = DriverManager.getConnection(JDBC_URL, DB_USER, DB_PASSWORD);
+
+            // Create a prepared statement to delete the employee with the specified ID.
+            String deleteQuery = "DELETE FROM employees WHERE id = ?";
+            PreparedStatement preparedStatement = connection.prepareStatement(deleteQuery);
+            preparedStatement.setInt(1, employeeId);
+
+            // Execute the prepared statement to delete the employee.
+            int rowsAffected = preparedStatement.executeUpdate();
+
+            if (rowsAffected > 0) {
+                System.out.println("Employee with ID " + employeeId + " has been removed from the database.");
+            } else {
+                System.out.println("Employee with ID " + employeeId + " not found in the database.");
+            }
+
+            // Close the resources.
+            preparedStatement.close();
+            connection.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+            // Handle any exceptions that may occur during database access.
+        }
 }
